38dc43194924255ddf9566385fc73ea07b379f95
Philemon <philemonariko@gmail.com>
2024-05-22 19:42:30 +0300

    dd

--- index.js
+++ index.js
@@ -9,14 +9,11 @@ app.post('/', (req, res) => {
     console.log(req.body.ref)
   const { ref } = req.body;
   if (ref === 'refs/heads/main') { // Check if the push is to the main branch
+    res.status(200).send('Deploying changes ...');
     exec(process.env.GROWTHSPRING_API_DEPLOY, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
-        return res.status(500).send('Error');
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-      res.status(200).send('Successfully deployed');
     });
   } else {
     res.status(200).send('No action taken');
